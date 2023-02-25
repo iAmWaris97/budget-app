afe35295f4762920da391a0c4c0205817b3e701d
iAmWaris97 <haleem.waris@gmail.com>
2023-02-26 02:10:19 +0430

    Add models associate them and validate them

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_25_213022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
+  create_table "group_expenses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.index ["expense_id"], name: "index_group_expenses_on_expense_id"
+    t.index ["group_id"], name: "index_group_expenses_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "expenses", "users"
+  add_foreign_key "group_expenses", "expenses"
+  add_foreign_key "group_expenses", "groups"
+  add_foreign_key "groups", "users"
 end
