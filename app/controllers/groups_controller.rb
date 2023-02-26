cecec512a0bc9e32f217e9b6f791fff4aa3553e7
iAmWaris97 <haleem.waris@gmail.com>
2023-02-26 05:11:06 +0430

    worked on group CRUD actions and views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,61 @@
 class GroupsController < ApplicationController
+  before_action :find_user
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @groups = @user.groups.all
+  end
+
+  def show
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = @user
+    if @group.save
+      redirect_to group_path(id: @group.id), notice: 'Group created successfully'
+    else
+      flash.now[:alert] = @group.errors.full_messages.first if @group.errors.any?
+      render :new, status: 400
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path(id: @group.id), notice: 'Group updated successfully'
+    else
+      flash.now[:alert] = @group.errors.full_messages.first if @group.errors.any?
+      render :edit, status: 400
+    end
+  end
+  
+  def destroy
+    if @group.destroy
+      redirect_to groups_path, notice: 'Groups was deleted successfully'
+    else
+      flash.now[:alert] = @group.errors.full_messages.first if @group.errors.any?
+      render :index, status: 400
+    end
+  end
+  
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def find_group
+    @group = Group.find_by_id(params[:id])
+  end
+  
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
