236f5475e6c23c9857b97701fad6f85208214185
iAmWaris97 <haleem.waris@gmail.com>
2023-02-26 05:43:48 +0430

    add expenses and route it to groups also creat menu

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,10 @@ class GroupsController < ApplicationController
 
   def index
     @groups = @user.groups.all
+    # @groups_sum = {}
+    # @groups.each do |group|
+    #   @groups_sum[group.id] = group.expenses.sum(:amount)
+    # end
   end
 
   def show
@@ -23,7 +27,7 @@ class GroupsController < ApplicationController
       render :new, status: 400
     end
   end
-  
+
   def edit
   end
 
@@ -35,8 +39,15 @@ class GroupsController < ApplicationController
       render :edit, status: 400
     end
   end
-  
+
   def destroy
+    @group = Group.find(params[:id])
+    @group_expenses = GroupExpense.where(group_id: @group.id)
+    @group_expenses.each do |group_expense|
+      expense_id = group_expense.expense_id
+      group_expense.destroy
+      expense = Expense.delete(expense_id)
+    end
     if @group.destroy
       redirect_to groups_path, notice: 'Groups was deleted successfully'
     else
@@ -44,7 +55,7 @@ class GroupsController < ApplicationController
       render :index, status: 400
     end
   end
-  
+
   private
 
   def find_user
@@ -54,7 +65,7 @@ class GroupsController < ApplicationController
   def find_group
     @group = Group.find_by_id(params[:id])
   end
-  
+
   def group_params
     params.require(:group).permit(:name, :icon)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :splash, only: [:index]
-  resources :groups
+  resources :groups do
+    resources :expenses
+  end
 
   # Defines the root path route ("/")
   root 'splash#index'
